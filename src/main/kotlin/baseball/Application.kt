f251f5c52cbac15e2cad14653a577a562b606afb
waterminn <tnals08019@gmail.com>
2023-01-25 20:11:12 +0900

    feat: generate random answer number

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,3 +3,17 @@ package baseball
 fun main() {
     playBaseball()
 }
+
+fun computerNum() : MutableList<Int> {
+    val com = mutableListOf<Int>()
+    while (com.size < 3) {
+        val num = Randoms.pickNumberInRange(1, 9)
+        if (!com.contains(num)) com.add(num) else continue
+    }
+    return com
+}
+
+fun playBaseball() {
+    print("숫자 야구 게임을 시작합니다.\n")
+    computerNum()
+}
