67cbc8c9048ab4ad1c932c92159e5c9968ff485b
waterminn <tnals08019@gmail.com>
2023-01-25 20:11:45 +0900

    feat: get user number input

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,4 +1,6 @@
 package baseball
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     playBaseball()
@@ -13,7 +15,34 @@ fun computerNum() : MutableList<Int> {
     return com
 }
 
+fun playerNum(): List<Int> {
+    var user: List<Int> = Console.readLine().map { it.toString().toInt() }
+
+    if (!user.isNullOrEmpty() && isValidInput(user)) {
+        return user
+    } else {
+        throw IllegalArgumentException("서로 다른 3자리의 숫자를 입력해주세요.")
+    }
+}
+
+fun isValidInput(input: List<Int>): Boolean {
+    if (input.size != 3) return false
+    for (i in input) {
+        val predicate: (Int) -> Boolean = { it == i }
+        if (input.count(predicate) > 1) return false
+    }
+    return true
+}
+
 fun playBaseball() {
     print("숫자 야구 게임을 시작합니다.\n")
+    print("숫자를 입력해주세요 : ")
     computerNum()
+    var input: List<Int> = listOf()
+    try {
+        input = playerNum()
+    } catch (e: IllegalArgumentException) {
+        print(e.message)
+    }
+    print("the input was ${input.joinToString("")}")
 }
